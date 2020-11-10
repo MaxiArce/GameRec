4436547746f87c92b23ef02790d4d49785722fe5
Maximiliano Arce <maximilianoarce@outlook.com>
2020-11-10 18:28:58 -0300

    add find matchs tags in games

--- js/index.js
+++ js/index.js
@@ -1,39 +1,21 @@
 // javascript del index
-// variables divs populares
 var popularContainers = document.querySelectorAll(".card-item");
-// variables de las tabs
+var recommendButton = document.getElementById("button__recommend")
+var searchButton = document.getElementById("button__search")
 var tabButtons = document.querySelectorAll(".button_select-tab");
 var tabContent = document.querySelectorAll(".tab-content");
-//variables slider años
 var slider = document.getElementById("range");
-var output  = document.getElementById("slider-text");
-
-//funcion para hacer una api request con los 5 juegos mas populares del 2020
-function fillPopularGames(){
-              
-        const apiUrl = "https://api.rawg.io/api/games?page_size=4&dates=2020-01-01,2020-12-31&ordering=-added"
-
-        //request obtiene el json y lo recibe un promise como objeto
-        fetch(apiUrl)
-            .then((data)=> data.json())
-            .then((dataObject)=>{      
-                var gameResults = dataObject.results;
-                //recorre el array y por cada juego recibido cambia el source de las img/nombre dentro del div
-                for (let i = 0; i < gameResults.length; i++){
-                    var image = popularContainers[i].getElementsByClassName("card-img");
-                    var title = popularContainers[i].getElementsByClassName("card-title"); 
-                    image.item(0).src = gameResults[i].background_image;
-                    title.item(0).innerHTML = gameResults[i].name;  
-                }
-            });
-
-}
+var output = document.getElementById("slider-text");
 
 //cambia el texto del slider con cada cambio del slider
 output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+//Agrega listeners a los bottones
+recommendButton.addEventListener("click", function() { showTabContent(0) });
+searchButton.addEventListener("click", function() { showTabContent(1) });
 
 //funcion ocultar/mostrar tabs
 function showTabContent(tab) {
@@ -44,6 +26,28 @@ function showTabContent(tab) {
     //hide all tabs except the selected one
     tabContent.forEach(element => element.style.display = "none");
     tabContent[tab].style.display = "block";
+}
+
+
+//funcion para hacer una api request con los 5 juegos mas populares del 2020
+function fillPopularGames() {
+
+    const apiUrl = "https://api.rawg.io/api/games?page_size=4&dates=2020-01-01,2020-12-31&ordering=-added"
+
+    //request obtiene el json y lo recibe un promise como objeto
+    fetch(apiUrl)
+        .then((data) => data.json())
+        .then((dataObject) => {
+            var gameResults = dataObject.results;
+            //recorre el array y por cada juego recibido cambia el source de las img/nombre dentro del div
+            for (let i = 0; i < gameResults.length; i++) {
+                var image = popularContainers[i].getElementsByClassName("card-img");
+                var title = popularContainers[i].getElementsByClassName("card-title");
+                image.item(0).src = gameResults[i].background_image;
+                title.item(0).innerHTML = gameResults[i].name;
+            }
+        });
+
 }
 
 fillPopularGames()
--- js/recommend.js
+++ js/recommend.js
@@ -1,8 +1,10 @@
 // javascript de la funcion principal - Para buscar o el usar el recomendador de juegos 
 window.onload = function() {
     var buttonSearch = document.getElementById("button__search--recommend").addEventListener("click", searchGame);
-    var gamesDataArray = [];
-    
+    var game1;
+    var game2;
+    var tagsMatch = [];
+
     // constructor objeto juego
     function Game(id, name, image, tags) {
         this.id = id;
@@ -12,58 +14,79 @@ window.onload = function() {
     }
 
     //recibe la informacion de los inputs y consulta la api
-    function searchGame() {
-        gamesDataArray = [];
+    async function searchGame() {
         var inputGame1 = document.getElementById("input-search--game1").value;
         var inputGame2 = document.getElementById("input-search--game2").value;
         //valida los inputs
         if (inputGame1 != "" && inputGame2 != "") {
-            getGameFromApi(inputGame1, 0)
-            getGameFromApi(inputGame2, 1)
-
+            game1 = await getGameFromApi("games", "search", inputGame1);
+            fillCard(game1, 0);
+            game2 = await getGameFromApi("games", "search", inputGame2);
+            fillCard(game2, 1);
+            findTagsMatch(game1, game2)
+            searchGameResults()
         } else {
             alert("Ingrese un juego en cada campo")
         }
     }
 
-    //busca en la api el juego por nombre y lo guarda en el array
-    function getGameFromApi(name, cardPosition) {
-
+    //busca en la api el juego por parametro y lo guarda
+    async function getGameFromApi(endPoint, qParameters, Input) {
         const apiData = {
             url: " https://api.rawg.io/api/",
-            endPoint: "games",
-            queryParameters: "search",
-            searchInput: name.replace(/\s/g, "%20")
+            endPoint: endPoint,
+            queryParameters: qParameters,
+            searchInput: Input.replace(/\s/g, "%20")
         }
         const apiUrl = `${apiData.url}${apiData.endPoint}?${apiData.queryParameters
         }=${apiData.searchInput}`;
-        console.log(apiUrl);
 
-        fetch(apiUrl)
-            .then((data) => data.json())
-            .then((dataObj) => {
-                var id = dataObj.results[0].id;
-                var name = dataObj.results[0].name;
-                var image = dataObj.results[0].background_image;
-                var tags = (dataObj.results[0].tags);
-                // console.log(dataObj.results[0].tags[0].id)
-                var game = new Game(id,name,image,tags)
-                gamesDataArray.push(game);
-                fillCard(cardPosition)
-            });
+        let dataObj = await fetch(apiUrl).then(resp => resp.json());
+        var id = dataObj.results[0].id;
+        var name = dataObj.results[0].name;
+        var image = dataObj.results[0].background_image;
+        var tags = (dataObj.results[0].tags);
+        var game = new Game(id, name, image, tags)
+        return game;
     }
+
     //Rellena los dos resultados de busqueda con la informacion del array de juegos //todo async?
-    function fillCard(cardPosition) {
+    function fillCard(game, cardPosition) {
         var card = document.getElementById("card-list-results");
         var cardName = document.getElementById(`card-title-results-${cardPosition}`);
         var cardImage = document.getElementById(`card-img-results-${cardPosition}`);
-        cardName.innerHTML = gamesDataArray[cardPosition].name;
-        cardImage.src = gamesDataArray[cardPosition].image;
+        cardName.innerHTML = game.name;
+        cardImage.src = game.image;
         card.style.visibility = "visible";
     }
 
-    // function findMatchingTags(){
-    //     console.log(gamesDataArray)
-    // }
-    // findMatchingTags()
-} 
+    //busca coincidencias  de tags entre los dos juegos
+    function findTagsMatch(game1, game2) {
+        tagsMatch = [];
+        var tagsGame1 = game1.tags;
+        var tagsGame2 = game2.tags;
+        tagsGame1.forEach(element1 => {
+            tagsGame2.forEach(element2 => {
+                if (element1.id == element2.id) {
+                    tagsMatch.push(element1)
+                }
+            });
+        });
+        console.log(tagsMatch)
+    }
+
+    //hace un request a la api con el id del juego por cada tag
+    function searchGameResults() {
+        tagsMatch.forEach(async element => {
+            var gameResult = await getGameFromApi("games", "tags", `${(element.id)}`);
+            addGameToResults(gameResult)
+        });
+    }
+
+    function addGameToResults(gameResult) {
+        var resultsContainer = document.getElementById("container__results--recommend");
+        var cardContainer = document.createElement('p');
+        cardContainer.innerHTML = gameResult.name;
+        resultsContainer.appendChild(cardContainer);
+    }
+}
