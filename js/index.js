0ce8154f0f2c19643e0bcfb6a175a024ef2ed9e8
Maximiliano Arce <maximilianoarce@outlook.com>
2020-11-19 12:14:48 -0300

    Remove search tab

--- js/index.js
+++ js/index.js
@@ -1,64 +1,41 @@
 // javascript del index
-var savedGamesButton = document.getElementById("navbar-saved-games");
-var popularContainers = document.querySelectorAll(".card-item");
-var recommendButton = document.getElementById("button__recommend")
-var searchButton = document.getElementById("button__search")
-var tabButtons = document.querySelectorAll(".button_select-tab");
-var tabContent = document.querySelectorAll(".tab-content");
-var slider = document.getElementById("range");
-var output = document.getElementById("slider-text");
-
-//cambia el texto del slider con cada cambio del slider
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
+var savedGamesButton = $("#navbar-saved-games");
+var popularContainers = $(".card-item");
+var recommendButton = $("#button__recommend");
+var searchButton = $("#button__search")
+var slider = $("#range");
+var output = $("#slider-text");
 
 //Agrega listeners a los bottones
-recommendButton.addEventListener("click", function() { showTabContent(0) });
-searchButton.addEventListener("click", function() { showTabContent(1) });
-savedGamesButton.addEventListener("click", showSavedGames);
+$(savedGamesButton).click(function() { showSavedGames() })
 
 //Lee el local storage y muestra los juegos guardados
-function showSavedGames(){
-    var savedGamesModal = document.getElementById("savedGamesModal");
+function showSavedGames() {
 
-    if (localStorage.getItem("SavedGames") == null){
-        alert("No hay juegos guardados")
-    }else{
-        var savedGames = JSON.parse(localStorage.getItem("SavedGames"))
-        savedGames.forEach(element =>{
-            var gameName = document.createElement('p');
-            gameName.innerHTML = element;
-            savedGamesModal.appendChild(gameName);
-        })
+    var savedGamesModal = $('#savedGamesModal')
+
+    if (savedGamesModal.is(":hidden")) {
+
+        savedGamesModal.show();
+
+        if (localStorage.getItem("SavedGames") == null) {
+            alert("No hay juegos guardados")
+        } else {
+            var savedGames = JSON.parse(localStorage.getItem("SavedGames"))
+            savedGames.forEach(element => {
+                console.log(element)
+                savedGamesModal.append("<p>" + element + "</p>");
+            })
+        }
+
+    } else {
+        savedGamesModal.hide();
+        savedGamesModal.empty()
     }
-
-    if (savedGamesModal.style.visibility === "hidden") {
-        savedGamesModal.style.visibility = "visible";
-      } else {
-        savedGamesModal.style.visibility = "hidden";
-        savedGamesModal.innerHTML = ""
-      }
-    
 }
-
-
-//funcion ocultar/mostrar tabs
-function showTabContent(tab) {
-    //make selected button white color
-    tabButtons.forEach(element => element.style.color = "grey");
-    tabButtons[tab].style.color = "white";
-
-    //hide all tabs except the selected one
-    tabContent.forEach(element => element.style.display = "none");
-    tabContent[tab].style.display = "block";
-}
-
 
 //funcion para hacer una api request con los 5 juegos mas populares del 2020
 function fillPopularGames() {
-
     const apiUrl = "https://api.rawg.io/api/games?page_size=4&dates=2020-01-01,2020-12-31&ordering=-added"
 
     //request obtiene el json y lo recibe un promise como objeto
@@ -75,6 +52,12 @@ function fillPopularGames() {
             }
         });
 
+}
+
+//cambia el texto del slider con cada cambio del slider
+output.innerHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
 }
 
 fillPopularGames()
